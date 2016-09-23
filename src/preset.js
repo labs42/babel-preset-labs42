306cb9fc603a88e55b277e6ad44e8cbdcfad6e5a
Alexander Antipin <alex@antipin.com>
2016-09-23 15:33:38 +0300

    Fix babel-plugin-transform-decorators-legacy module import

--- src/preset.js
+++ src/preset.js
@@ -1,10 +1,10 @@
 module.exports = {
     plugins: [
-        'babel-plugin-transform-es2015-modules-commonjs',
-        'babel-plugin-transform-async-to-generator',
-        'babel-plugin-transform-decorators-legacy',
-        'babel-plugin-transform-class-properties',
-        'babel-plugin-transform-es2015-destructuring',
-        'babel-plugin-transform-object-rest-spread',
-    ].map(require)
+        require('babel-plugin-transform-es2015-modules-commonjs'),
+        require('babel-plugin-transform-async-to-generator'),
+        require('babel-plugin-transform-decorators-legacy').default,
+        require('babel-plugin-transform-class-properties'),
+        require('babel-plugin-transform-es2015-destructuring'),
+        require('babel-plugin-transform-object-rest-spread'),
+    ]
 }
